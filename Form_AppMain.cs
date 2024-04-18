6dc2509bd1aef934e2e917835be75160cfc9b83e
Kyle Cwik <kylecwik@gmail.com>
2024-04-18 16:17:39 -0500

    feat: added form AppMain

---
+++ Form_AppMain.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExerciseApp
+{
+    public partial class Form_AppMain : Form
+    {
+        public Form_AppMain()
+        {
+            InitializeComponent();
+        }
+    }
+}

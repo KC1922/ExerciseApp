05ea02e6a75f0708d33996fc72331773687fab0a
Kyle Cwik <kylecwik@gmail.com>
2024-04-18 14:19:41 -0500

    feat: added sql insertion of workout values

--- Form_HomeWindow.cs
+++ Form_HomeWindow.cs
@@ -1,4 +1,5 @@
-﻿using Npgsql;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using Npgsql;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -89,7 +90,76 @@ namespace ExerciseApp
 
         private void button_Add_Click(object sender, EventArgs e)
         {
+            int exercise = comboBox_ExerciseList.SelectedIndex;
+            string calories = textBox_Calories.Text;
+            string avgPace = textBox_AvgPace.Text;
+            string distance = textBox_Distance.Text;
+            string avgSpeed = textBox_AvgSpeed.Text;
+            string maxHr = textBox_MaxHR.Text;
+            string minHr = textBox_MinHR.Text;
+            string date = dateTimePicker_Date.Text;
+            string startTime = dateTimePicker_StartTime.Text;
+            string duration = dateTimePicker_Duration.Text;
 
+            using (NpgsqlConnection connection = new NpgsqlConnection(dbConnection))
+            {
+                connection.Open();
+
+  
+                // Begin a transaction as code inserts values into two tables
+                using (NpgsqlTransaction transaction = connection.BeginTransaction())
+                {
+                    try
+                    {
+                        string insertWorkoutQuery = @"INSERT INTO workouts (user_id, exercise_id, calories_burned, max_hr, min_hr, distance, avg_pace, avg_speed)
+                                   VALUES (@userId, @exercise, @calories_burned, @max_hr, @min_hr, @distance, @avg_pace, @avg_speed)
+                                   RETURNING workout_id";
+
+                        int workout_id;
+
+                        using (NpgsqlCommand command = new NpgsqlCommand(insertWorkoutQuery, connection))
+                        {
+                            command.Parameters.AddWithValue("@exercise", exercise);
+                            command.Parameters.AddWithValue("@calories_burned", string.IsNullOrEmpty(calories) ? 0 : int.Parse(calories));
+                            command.Parameters.AddWithValue("@max_hr", string.IsNullOrEmpty(maxHr) ? 0: int.Parse(maxHr));
+                            command.Parameters.AddWithValue("@min_hr", string.IsNullOrEmpty(minHr) ? 0 : int.Parse(maxHr));
+                            command.Parameters.AddWithValue("@distance", string.IsNullOrEmpty(distance) ? 0 : int.Parse(maxHr));
+                            command.Parameters.AddWithValue("@avg_pace", string.IsNullOrEmpty(avgPace) ? 0 : int.Parse(maxHr));
+                            command.Parameters.AddWithValue("@avg_speed", string.IsNullOrEmpty(avgSpeed) ? 0 : int.Parse(maxHr));
+
+                            // Execute the command and retrieve the userid of the newly inserted user
+                            workout_id = (int)command.ExecuteScalar();
+
+                            if (workout_id == 0)
+                            {
+                                throw new Exception("Failed to add workout details. Make sure entered information is correct.");
+                            }
+
+                            // Insert into UserPhysicalDetails table
+                            string insertTimingQuery = @"INSERT INTO timing (workout_id, date, start_time, duration)
+                                                       VALUES (@workout_id, @date, @start_time, @duration)";
+                            using (NpgsqlCommand commandInsertTimingQuery = new NpgsqlCommand(insertWorkoutQuery, connection))
+                            {
+                                commandInsertTimingQuery.Parameters.AddWithValue("@workout_id", workout_id);
+                                commandInsertTimingQuery.Parameters.AddWithValue("@date", date);
+                                commandInsertTimingQuery.Parameters.AddWithValue("@start_time", startTime);
+                                try
+                                {
+                                    commandInsertTimingQuery.Parameters.AddWithValue("@duration", duration);
+                                }
+                                catch (FormatException) { throw new Exception("Enter a valid duration."); }
+                            }
+                        }
+                        transaction.Commit();
+                        MessageBox.Show("Workout added.");
+                    }
+                    catch (Exception ex) 
+                    {
+                        transaction.Rollback();
+                        MessageBox.Show("Error adding workout: " + ex.Message);
+                    }
+                }
+            }
         }
     }
 }
